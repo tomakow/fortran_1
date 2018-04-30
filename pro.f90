2699d05c1196a9c84e7c0ba81e9a29abf06c1bf6
tomakow <thoren999@gmail.com>
2018-04-30 15:48:58 +0200

    przeniesienie subrutyn do osobnego modułu

--- pro.f90
+++ pro.f90
@@ -1,19 +1,59 @@
+module subrutyny
+contains
+	subroutine fill_table_1 (table, size)
+	implicit none
+	integer (kind=4) :: size
+	real (kind=4) :: table(size)
+
+	call random_number(table)
+
+	!write (*,*) table
+
+	end subroutine
+
+
+	subroutine fill_table_2 (table, size1, size2)
+	implicit none
+	integer (kind=4) :: size1, size2
+	real (kind=4) :: table(size1, size2)
+
+	call random_number(table)
+
+	!write (*,*) table
+
+	end subroutine
+end module subrutyny
+
+
+
 program main
+
+use subrutyny
 
 implicit none
 
-integer (kind=4) :: i, j, c, test
-real (kind=8), allocatable, dimension(:,:) :: a
-real (kind=8), allocatable, dimension(:) :: x
+integer (kind=4) :: i, j, iSize
+real (kind=4) :: c
+real (kind=4), allocatable, dimension(:,:) :: a
+real (kind=4), allocatable, dimension(:) :: x
 
-test = 10
-
-allocate (a(test,test))
-allocate (x(test))
+iSize = 10
 
 
-do i=1,test
-	do j=1,test
+! ALOKOWANIE TABLIC
+
+allocate (a(iSize,iSize))
+allocate (x(iSize))
+
+! WYPEŁNIANIE TABLIC
+
+call fill_table_1 (x, iSize)
+call fill_table_2 (a, iSize, iSize)
+
+! ALGORYTM OBLICZENIOWY
+
+do i=1,iSize
+	do j=1,iSize
 		if (i .ne. j) then
 			c = ( a(i,j) / a(i,i) )
 				a(:,j) = a(:,j) - c*a(:,i)
@@ -24,9 +64,10 @@ do i=1,test
 	end do
 end do
 
-write (*,*) size(a(:,:))
-write (*,*) size(x)
+!write (*,*) size(a(:,:))
+!write (*,*) size(x)
 
-print *, c
+write (*,*) a
+write (*,*) x
 
 end program
